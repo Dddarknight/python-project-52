cffb1b5ce72808ca114c7464c41b80c0f625109e
Dddarknight <9801677@gmail.com>
2022-08-26 21:40:29 +0300

    step1

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+DATABASES = {'default': dj_database_url.config(default='postgres://...')}
 
 
 # Password validation
