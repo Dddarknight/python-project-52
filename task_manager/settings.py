48f6aa2898496c52dd5d7f5c06f6d162c9b58ba1
Dddarknight <9801677@gmail.com>
2022-08-26 23:43:32 +0300

    step1

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,6 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 
 ALLOWED_HOSTS = ['webserver']
+
+SECRET_KEY = os.environ('SECRET_KEY')
 
 
 # Application definition
