c5ae17a50d2de9a37768049a51583e7cf46183f7
Dddarknight <9801677@gmail.com>
2022-08-26 23:50:25 +0300

    step1

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import dj_database_url
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 
 ALLOWED_HOSTS = ['webserver']
-
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # Application definition
